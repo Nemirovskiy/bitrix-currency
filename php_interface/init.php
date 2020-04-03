befef308092827e03ed721137267323508bd9885
nemirovv <it-nikola@mail.ru>
2020-04-03 15:11:01 +0300

    Подключение в автозагрузку класса для работы с Заказами Поключение файла с регистратором обработчиков событий

--- php_interface/init.php
+++ php_interface/init.php
@@ -1,5 +1,8 @@
 <?
 $arClass = array(
 	"\\Nemin\\Helper" => '/local/lib/Nemin/Helper.php',
+	"\\Nemin\\Order" => '/local/lib/Nemin/Order.php',
 );
 Bitrix\Main\Loader::registerAutoLoadClasses(null, $arClass);
+$root = Bitrix\Main\Context::getCurrent()->getServer()->getDocumentRoot();
+require $root."/local/php_interface/include/events.php";
